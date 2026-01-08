b2ead5ab4406813b276ab7a52fc6ba7f3bd947a4
Uri Darom <uridarom@gmail.com>
2026-01-08 13:34:24 -0500

    Added motor control

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("Hello World")
+from raven import Raven
+
+raven_board = Raven()
+
+raven_board.set_motor_mode(Raven.MotorChannel.CH1, Raven.MotorMode.DIRECT) # Set motor mode to DIRECT
+
+# Speed controlled:
+raven_board.set_motor_torque_factor(Raven.MotorChannel.CH1, 100) # Let the motor use all the torque to get to speed factor
+raven_board.set_motor_speed_factor(Raven.MotorChannel.CH1, 10, reverse=True) # Spin at 10% max speed in reverse
+
+# Torque controlled:
+raven_board.set_motor_speed_factor(Raven.MotorChannel.CH1, 100) # Make motor try to run at max speed forward
+raven_board.set_motor_torque_factor(Raven.MotorChannel.CH1, 10) # Let it use up to 10% available torque
