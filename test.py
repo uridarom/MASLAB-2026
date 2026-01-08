335a4af6f0fa479423ef816e67ae97641be73026
Uri Darom <uridarom@gmail.com>
2026-01-08 14:57:32 -0500

    Added keyboard control

--- test.py
+++ test.py
@@ -1,20 +1,37 @@
+import curses
 from raven import Raven
 
 CHANNEL_1 = Raven.MotorChannel.CH1
 CHANNEL_2 = Raven.MotorChannel.CH2
 
-raven_board = Raven()
+raven = Raven()
 
-# Set both motors to DIRECT mode
-raven_board.set_motor_mode(CHANNEL_1, Raven.MotorMode.DIRECT)
-raven_board.set_motor_mode(CHANNEL_2, Raven.MotorMode.DIRECT)
+for ch in (CHANNEL_1, CHANNEL_2):
+    raven.set_motor_mode(ch, Raven.MotorMode.DIRECT)
+    raven.set_motor_torque_factor(ch, 100)
 
-# Motor 1
-raven_board.set_motor_torque_factor(CHANNEL_1, 100)
-raven_board.set_motor_speed_factor(CHANNEL_1, 30)
+def main(stdscr):
+    stdscr.nodelay(True)
+    stdscr.clear()
 
-# Motor 2
-raven_board.set_motor_torque_factor(CHANNEL_2, 100)
-raven_board.set_motor_speed_factor(CHANNEL_2, 30, reverse=True)
-while True:
-    pass
+    while True:
+        key = stdscr.getch()
+
+        if key == ord('w'):
+            raven.set_motor_speed_factor(CHANNEL_1, 30)
+            raven.set_motor_speed_factor(CHANNEL_2, 30, reverse=True)
+        elif key == ord('s'):
+            raven.set_motor_speed_factor(CHANNEL_1, 30, reverse=True)
+            raven.set_motor_speed_factor(CHANNEL_2, 30)
+        elif key == ord('a'):
+            raven.set_motor_speed_factor(CHANNEL_1, 20)
+            raven.set_motor_speed_factor(CHANNEL_2, 20)
+        elif key == ord('d'):
+            raven.set_motor_speed_factor(CHANNEL_1, 20, reverse=True)
+            raven.set_motor_speed_factor(CHANNEL_2, 20, reverse=True)
+        elif key == ord('q'):
+            raven.set_motor_speed_factor(CHANNEL_1, 0)
+            raven.set_motor_speed_factor(CHANNEL_2, 0)
+            break
+
+curses.wrapper(main)
